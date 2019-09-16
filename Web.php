7b3b3e2969aafe050cd7dc3f52d4d7ec80468beb
r18020152 <florent.reymond@etu.univ-amu.fr>
2019-09-16 16:20:05 +0200

    TP1

--- Web.php
+++ Web.php
@@ -1,8 +1,12 @@
 <?php
-    $var1 = 6;
-    $var2 = 1.3;
-    $var3 = 'Variable 3';
+$var1 = 6;
+$var2 = 1.3;
+$var3 = 'Variable 3';
 ?>
+
+<?php echo "$var1 + $var2";?>
+<?php echo $var1 + $var2;?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,5 +20,5 @@
 </html>
 
 <?php   echo'<strong> Voici mon premier programme PHP </strong><br/>'."\n";
-        echo 'C\'est pas mal non ? <br/>';
-        ?>
+echo 'C\'est pas mal non ? <br/>';
+?>
