a486d68442f0383adfb55c5021aac7b60d64d7ac
r18020152 <florent.reymond@etu.univ-amu.fr>
2019-09-16 16:38:07 +0200

    TP1

--- Web.php
+++ Web.php
@@ -1,4 +1,18 @@
 <?php
+function start_page($title)
+{
+    echo '<!DOCTYPE html><html
+        lang="fr"><head><title>' . PHP_EOL . $title . '</title></head>>body>' . PHP_EOL
+    ;
+
+
+};
+?>
+
+<?php
+start_page('titre');
+?>
+<?php
 $var1 = 6;
 $var2 = 1.3;
 $var3 = 'Variable 3';
@@ -7,17 +21,6 @@ $var3 = 'Variable 3';
 <?php echo "$var1 + $var2";?>
 <?php echo $var1 + $var2;?>
 <?php echo $var1 + $var3; ?>
-<!DOCTYPE html>
-<html>
-<head>
-
-    <meta charset="utf-8" />
-    <title>Titre</title>
-</head>
-
-<body>
-</body>
-</html>
 
 <?php   echo'<strong> Voici mon premier programme PHP </strong><br/>'."\n";
 echo 'C\'est pas mal non ? <br/>';
