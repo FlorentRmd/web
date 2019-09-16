c717faa67da3f68be772c5a347a9617d85c707fe
r18020152 <florent.reymond@etu.univ-amu.fr>
2019-09-16 16:08:05 +0200

    Test

---
+++ Web.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- En-tête de la page -->
+    <meta charset="utf-8" />
+    <title>Titre</title>
+</head>
+
+<body>
+<!-- Corps de la page -->
+</body>
+</html>
+
+<?php   echo <'strong> Voici mon premier programme PHP </strong><br/>'."\n";
+        echo 'C\'est pas mal non ? <br/>';
+        ?>
